60958aebb9e70316b64d9a0073d7be6be72fe0ae
dcordon11 <dacordon@url.edu.gt>
2026-02-25 13:33:00 -0600

    Update App.py

--- app.py
+++ app.py
@@ -1,1 +1,4 @@
-print("Hola desde Jenkins (en Python)")
+print("Hola desde Jenkins!  Soy Diego!")
+print("Este es el laboratorio #3 de Ingeniería de Software 2")
+
+input("Presiona ENTER para cerrar...")
